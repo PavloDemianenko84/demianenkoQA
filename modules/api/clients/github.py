6a79323eef23960cb92d46635a0f3bd7a8cc2eea
Pavlo Demianenko <pavlo.demianenko84@gmail.com>
2023-07-23 17:59:57 +0300

    Test api client

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -3,15 +3,11 @@ import requests
 
 class GitHub:
 
-    def get_user_defunct(self):
-        r = requests.get('https://api.github.com/users/defunkt')
+    def get_user(self, username):
+        r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
         return body
-    
-    def get_not_exist_user(self):
-        r = requests.get('https://api.github.com/users/butenkosergii')
-        body = r.json()
 
-        return body
+        
     
